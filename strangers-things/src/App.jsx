6f56971508fd2e7241abb4ffe77013d2761008dd
sktisdale <sktisdale@gmail.com>
2023-08-14 20:06:35 -0600

    imported NewPost

--- strangers-things/src/App.jsx
+++ strangers-things/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import Posts from './components/DisplayPosts'
+import NewPost from './components/NewPost'
 
 function App() {
   return (
     <>
+      <NewPost token={token}/>
       <Posts/>
     </>
   )
