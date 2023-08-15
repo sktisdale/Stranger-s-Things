4eb5850058fa9856feb77127edf1fe0771fce76b
tmendoza27 <trevormendoza2002@gmail.com>
2023-08-14 18:35:25 -0700

    Displays posts with new file in components folder

---
+++ strangers-things/src/components/DisplayPosts.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react"
+
+const COHORT_NAME = '2305-FTB-MT-WEB-PT'
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+
+export default function Posts(){
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(()=>{
+        async function getPosts(){
+            try {
+                const response = await fetch(`${BASE_URL}/posts`)
+                const result = await response.json();
+                console.log(result);
+                setPosts(result.data.posts)
+                return result
+              } catch (err) {
+                console.error(err);
+              }
+        }
+
+        getPosts()
+
+    }, [])
+    
+
+
+    return(<>   
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+            posts.map((post)=>{
+                return (
+                    <tr key={post._id}>
+                        <td>{post.title}</td>
+                        <td>{post.description}</td>
+                        <td>{post.price}</td>
+                    </tr>
+                )
+            })
+            }
+        </tbody>
+    </table>
+      </>
+    )
+  }
